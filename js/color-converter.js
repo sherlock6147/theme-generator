1cec482329f92c9b66895e9778f9bb94faf1eb41
md manzer alam <tooshort9541@gmail.com>
2021-03-08 12:46:01 +0530

    color conversion part completed

---
+++ js/color-converter.js
@@ -0,0 +1,126 @@
+// input elements
+var hexInput = document.getElementById('hex-input');
+var rInput = document.getElementById('r-input');
+var gInput = document.getElementById('g-input');
+var bInput = document.getElementById('b-input');
+var hInput = document.getElementById('h-input');
+var sInput = document.getElementById('s-input');
+var lInput = document.getElementById('l-input');
+var colorBg = document.getElementById('color-box');
+var colorValue = document.getElementById('hex-code-output');
+
+let color = new Color();
+function hexFieldChanged() {
+    console.log("changed "+hexInput.value);
+    if (hexInput.value.length == 3) {
+        // TODO convert to rgb
+        // TODO convert to hsl
+        // TODO change the color of the color-block
+        let temp = hexInput.value;
+        if (parseInt(temp[0] + temp[0], 16) < 256 && parseInt(temp[1] + temp[1], 16) < 256 && parseInt(temp[2] + temp[2], 16) < 256) {
+            color.hex = "#" + temp[0] + temp[0] + temp[1] + temp[1] + temp[2] + temp[2];
+            console.log(color.hex);
+            color.updateFromHex();
+            colorBg.style.backgroundColor = color.hex;
+            colorValue.innerHTML = color.hex;
+            rInput.value = color.r;
+            gInput.value = color.g;
+            bInput.value = color.b;
+            hInput.value = color.h;
+            sInput.value = color.s;
+            lInput.value = color.l;
+        }
+    } else if (hexInput.value.length == 6) {
+        // TODO convert to rgb
+        // TODO convert to hsl
+        // TODO change the color of the color-block
+        let temp = hexInput.value;
+        if (parseInt(temp[0] + temp[1], 16) < 256 && parseInt(temp[2] + temp[3], 16) < 256 && parseInt(temp[4] + temp[5], 16) < 256) {
+            color.hex = "#" + temp[0] + temp[1] + temp[2] + temp[3] + temp[4] + temp[5];
+            console.log(color.hex);
+            color.updateFromHex();
+            colorBg.style.backgroundColor = color.hex;
+            colorValue.innerHTML = color.hex;
+            rInput.value = color.r;
+            gInput.value = color.g;
+            bInput.value = color.b;
+            hInput.value = color.h;
+            sInput.value = color.s;
+            lInput.value = color.l;
+        }
+    }
+    else {
+        // do nothing
+    }
+}
+function rgbFieldChanged() {
+    color.r = parseInt(rInput.value);
+    color.g = parseInt(gInput.value);
+    color.b = parseInt(bInput.value);
+    color.updateFromRGB();
+    colorBg.style.backgroundColor = color.hex;
+    colorValue.innerHTML = color.hex;
+    let te = color.hex.split("#");
+    hexInput.value = te[1];
+    hInput.value = color.h;
+    sInput.value = color.s;
+    lInput.value = color.l;
+}
+function hslFieldChanged() {
+    color.h = parseInt(hInput.value);
+    color.s = parseInt(sInput.value);
+    color.l = parseInt(lInput.value);
+    color.updateFromHSL();
+    colorBg.style.backgroundColor = color.hex;
+    colorValue.innerHTML = color.hex;
+    let te = color.hex.split("#");
+    hexInput.value = te[1];
+    rInput.value = color.r;
+    gInput.value = color.g;
+    bInput.value = color.b;
+}
+$('#hex-input').on('input', function (e) {
+    hexFieldChanged();
+});
+$('#r-input').on('input', function (e) {
+    if (rInput.value != "" || gInput.value != "" || bInput.value != "") {
+        if (parseInt(rInput.value) < 256 && parseInt(gInput.value) < 256 && parseInt(bInput.value) < 256) {
+            rgbFieldChanged();
+        }
+    }    
+});
+$('#g-input').on('input', function (e) {
+    if (rInput.value != "" || gInput.value != "" || bInput.value != "") {
+        if (parseInt(rInput.value) < 256 && parseInt(gInput.value) < 256 && parseInt(bInput.value) < 256) {
+            rgbFieldChanged();
+        }
+    }    
+});
+$('#b-input').on('input', function (e) {
+    if (rInput.value != "" || gInput.value != "" || bInput.value != "") {
+        if (parseInt(rInput.value) < 256 && parseInt(gInput.value) < 256 && parseInt(bInput.value) < 256) {
+            rgbFieldChanged();
+        }
+    }    
+});
+$('#h-input').on('input', function (e) {
+    if (hInput.value != "" || sInput.value != "" || lInput.value != "") {
+        if (parseInt(hInput.value) >0 && parseInt(sInput.value) > 0 && parseInt(lInput.value) >0) {
+            hslFieldChanged();
+        }
+    }    
+});
+$('#s-input').on('input', function (e) {
+    if (hInput.value != "" || sInput.value != "" || lInput.value != "") {
+        if (parseInt(hInput.value) >0 && parseInt(sInput.value) > 0 && parseInt(lInput.value) >0) {
+            hslFieldChanged();
+        }
+    }    
+});
+$('#l-input').on('input', function (e) {
+    if (hInput.value != "" || sInput.value != "" || lInput.value != "") {
+        if (parseInt(hInput.value) >0 && parseInt(sInput.value) > 0 && parseInt(lInput.value) >0) {
+            hslFieldChanged();
+        }
+    }    
+});
---
+++ js/color.js
@@ -0,0 +1,108 @@
+class Color{
+    // Setup
+    constructor() {
+        this.hex = "#EDEDED";
+        this.r = parseInt(this.hex[1] + this.hex[2], 16);
+        this.g = parseInt(this.hex[3] + this.hex[4], 16);
+        this.b = parseInt(this.hex[5] + this.hex[6], 16);
+        this.h = 0;
+        this.s = 0;
+        this.l = 94;
+        console.log(this.r,this.g,this.b);
+    }
+    rgbToHsl() {
+        let r = this.r, g = this.g, b = this.b;
+        r /= 255;
+        g /= 255;
+        b /= 255;
+        let cmin = Math.min(r,g,b),
+            cmax = Math.max(r,g,b),
+            delta = cmax - cmin,
+            h = 0,
+            s = 0,
+            l = 0;
+
+        if (delta == 0)
+            h = 0;
+        else if (cmax == r)
+            h = ((g - b) / delta) % 6;
+        else if (cmax == g)
+            h = (b - r) / delta + 2;
+        else
+            h = (r - g) / delta + 4;
+
+        h = Math.round(h * 60);
+
+        if (h < 0)
+            h += 360;
+
+        l = (cmax + cmin) / 2;
+        s = delta == 0 ? 0 : delta / (1 - Math.abs(2 * l - 1));
+        s = +(s * 100).toFixed(1);
+        l = +(l * 100).toFixed(1);
+        this.h = h;
+        this.s = s;
+        this.l = l;
+        console.log(this.h, this.s, this.l);
+    }
+    updateFromHex() {
+        this.r = parseInt(this.hex[1] + this.hex[2], 16);
+        this.g = parseInt(this.hex[3] + this.hex[4], 16);
+        this.b = parseInt(this.hex[5] + this.hex[6], 16);
+        console.log(this.r,this.g,this.b);
+        this.rgbToHsl();
+    }
+    rgbToHex() {
+        let r = 0, g = 0, b = 0;
+        r = this.r.toString(16);
+        g = this.g.toString(16);
+        b = this.b.toString(16);
+        if (r.length == 1)
+            r = "0" + r;
+        if (g.length == 1)
+            g = "0" + g;
+        if (b.length == 1)
+            b = "0" + b;
+        this.hex = "#" + r + g + b;
+        console.log(this.hex);
+    }
+    hslToRgb() {
+        let s = 0, l = 0, h = 0;
+        s = this.s/100;
+        l = this.l/100;
+        h = this.h;
+        let c = (1 - Math.abs(2 * l - 1)) * s,
+            x = c * (1 - Math.abs((h / 60) % 2 - 1)),
+            m = l - c/2,
+            r = 0,
+            g = 0,
+            b = 0;
+        if (0 <= h && h < 60) {
+            r = c; g = x; b = 0;  
+            } else if (60 <= h && h < 120) {
+            r = x; g = c; b = 0;
+            } else if (120 <= h && h < 180) {
+            r = 0; g = c; b = x;
+            } else if (180 <= h && h < 240) {
+            r = 0; g = x; b = c;
+            } else if (240 <= h && h < 300) {
+            r = x; g = 0; b = c;
+            } else if (300 <= h && h < 360) {
+            r = c; g = 0; b = x;
+            }
+        r = Math.round((r + m) * 255);
+        g = Math.round((g + m) * 255);
+        b = Math.round((b + m) * 255);
+        this.r = r;
+        this.g = g;
+        this.b = b;
+    }
+    updateFromRGB() {
+        this.rgbToHex();
+        this.rgbToHsl();
+    }
+    updateFromHSL() {
+        this.hslToRgb();
+        this.rgbToHex();
+    }
+}
